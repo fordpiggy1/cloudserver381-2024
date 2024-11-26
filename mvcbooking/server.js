71d0cc1e03f6330e5da8104d28a022e6c08bbd59
fordpiggy1 <114170991+fordpiggy1@users.noreply.github.com>
2024-11-26 16:30:00 +0800

    Update server.js

--- mvcbooking/server.js
+++ mvcbooking/server.js
@@ -1,192 +1,130 @@
-/* 
-Controllers - express modules
------------------------------
-express-formiddable: https://www.npmjs.com/package/express-formidable
-- express-formidable can basically parse form types, including application/x-www-form-urlencoded, application/json, and multipart/form-data.
------------------------------
-fs/promises: https://nodejs.org/zh-tw/learn/manipulating-files/reading-files-with-nodejs
------------------------------
-*/
 const express = require('express');
+const fs = require('fs/promises');
+const formidable = require('express-formidable');
+const { MongoClient, ObjectId } = require("mongodb");
+
 const app = express();
-const port = process.env.PORT || 8099;
-const fs = require('node:fs/promises');
-const formidable = require('express-formidable'); 
+const port = process.env.PORT || 10000; // Use the PORT environment variable or default to 10000
+
+// Middleware
 app.use(formidable());
 
-/* Model - mongodb modules
-mongodb ^6.9: https://www.npmjs.com/package/mongodb
-*/
-const { MongoClient, ObjectId } = require("mongodb");
-const mongourl = '';
-const client = new MongoClient(mongourl); 
+// MongoDB setup
+const mongourl = ''; // Replace with your MongoDB connection string
+const client = new MongoClient(mongourl);
 const dbName = 'test';
 const collectionName = "bookings";
 
 // Views
 app.set('view engine', 'ejs');
 
+// Helper functions for database operations
 const findDocument = async (db, criteria) => {
-	let findResults = [];
-	let collection = db.collection(collectionName);
-	console.log(`findCriteria: ${JSON.stringify(criteria)}`);
-   	findResults = await collection.find(criteria).toArray();
-	console.log(`findDocument: ${findResults.length}`);
-	console.log(`findResults: ${JSON.stringify(findResults)}`);
-	return findResults;
+    const collection = db.collection(collectionName);
+    return await collection.find(criteria).toArray();
 };
 
 const updateDocument = async (db, criteria, updateDoc) => {
-    let updateResults = [];
-	let collection = db.collection(collectionName);
-	console.log(`updateCriteria: ${JSON.stringify(criteria)}`);
-   	updateResults = await collection.updateOne(criteria,{$set : updateDoc});
-	console.log(`updateResults: ${JSON.stringify(updateResults)}`);
-	return updateResults;
-}
+    const collection = db.collection(collectionName);
+    return await collection.updateOne(criteria, { $set: updateDoc });
+};
 
+// Route handlers
 const handle_Find = async (res, criteria) => {
-	await client.connect();
-	console.log("Connected successfully to server");
-	const db = client.db(dbName);
-    const docs = await findDocument(db, criteria);
-	await client.close();
-    console.log("Closed DB connection");
-    res.status(200).render('list',{nBookings: docs.length, bookings: docs});
-    /* list.ejs
-	<html>
-		<body>
-		    <H2>Bookings (<%= nBookings %>)</H2>
-		    <ul>
-		        <% for (var b of bookings) { %>
-		        <li>Booking ID: <a href="/details?_id=<%= b._id %>"><%= b.bookingid %></a></li>
-		        <% } %>
-		    </ul>
-		</body>
-	</html>
-		*/
-}
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const docs = await findDocument(db, criteria);
+        res.status(200).render('list', { nBookings: docs.length, bookings: docs });
+    } catch (error) {
+        console.error("Error finding documents:", error);
+        res.status(500).send("Internal Server Error");
+    } finally {
+        await client.close();
+    }
+};
 
 const handle_Details = async (res, criteria) => {
-    await client.connect();
-	console.log("Connected successfully to server");
-	const db = client.db(dbName);
-    /* use Document ID for query */
-    let DOCID = {};
-    DOCID['_id'] = new ObjectId(criteria._id);
-	const docs = await findDocument(db, DOCID); 
-	await client.close();
-    console.log("Closed DB connection");
-    res.status(200).render('details', {booking: docs[0]});
-    /* details.ejs
-	<html>
-		<body>
-		    <H2>Booking Details</H2><hr>
-		    <p>Booking ID: <%= booking.bookingid %></p>
-		    <p>Mobile: <%= booking.mobile %></p>
-
-		    <% if (booking.photo) { %>
-		        <img src="data:image/jpg;base64, <%= booking.photo %>"><br>
-		    <% } %>
-
-		    <a href="/edit?_id=<%= booking._id %>">edit</a><br>
-		    <a href="/">home</a>
-		</body>
-	</html>
-    */
-}
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const DOCID = { '_id': new ObjectId(criteria._id) };
+        const docs = await findDocument(db, DOCID);
+        res.status(200).render('details', { booking: docs[0] });
+    } catch (error) {
+        console.error("Error retrieving details:", error);
+        res.status(500).send("Internal Server Error");
+    } finally {
+        await client.close();
+    }
+};
 
 const handle_Edit = async (res, criteria) => {
-    await client.connect();
-	console.log("Connected successfully to server");
-	const db = client.db(dbName);
-    /* use Document ID for query */
-    let DOCID = {};
-    DOCID['_id'] = new ObjectId(criteria._id)
-	const docs = await findDocument(db, DOCID); 
-	await client.close();
-    console.log("Closed DB connection");
-    res.status(200).render('edit',{booking: docs[0]});
-    /* edit.ejs
-	<html>
-		<body>
-		    <form action="/update" method="POST" enctype="multipart/form-data">
-		        Booking ID: <input name="bookingid" value=<%= booking.bookingid %>><br>
-		        Mobile: <input name="mobile" value=<%= booking.mobile %> /><br>
-		        <input type="file" name="filetoupload"><br>
-		        <input type="hidden" name="_id" value=<%= booking._id %>>
-		        <input type="submit" value="update">  
-		    </form>
-		</body>
-	</html>
-    */
-}
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const DOCID = { '_id': new ObjectId(criteria._id) };
+        const docs = await findDocument(db, DOCID);
+        res.status(200).render('edit', { booking: docs[0] });
+    } catch (error) {
+        console.error("Error retrieving edit data:", error);
+        res.status(500).send("Internal Server Error");
+    } finally {
+        await client.close();
+    }
+};
 
 const handle_Update = async (req, res, criteria) => {
-	await client.connect();
-	console.log("Connected successfully to server");
-	const db = client.db(dbName);
-        var DOCID = {};
-        DOCID['_id'] = new ObjectId(req.fields._id);
-        var updateDoc = {};
-        updateDoc['bookingid'] = req.fields.bookingid;
-        updateDoc['mobile'] = req.fields.mobile;
-        if (req.files.filetoupload.size > 0) {
-			const data = await fs.readFile(req.files.filetoupload.path, { encoding: 'base64' });
-			updateDoc['photo'] = new Buffer.from(data);
-            const results = await updateDocument(db, DOCID, updateDoc);
-			await client.close();
-    		console.log("Closed DB connection");
-			res.status(200).render('info', {message: `Updated ${results.modifiedCount} document(s)`})
-        } else {
-            const results = await updateDocument(db, DOCID, updateDoc);
-			await client.close();
-    		console.log("Closed DB connection");
-			res.status(200).render('info', {message: `Updated ${results.modifiedCount} document(s)`})
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const DOCID = { '_id': new ObjectId(req.fields._id) };
+        const updateDoc = {
+            bookingid: req.fields.bookingid,
+            mobile: req.fields.mobile,
+        };
+
+        if (req.files.filetoupload && req.files.filetoupload.size > 0) {
+            const data = await fs.readFile(req.files.filetoupload.path, { encoding: 'base64' });
+            updateDoc.photo = Buffer.from(data);
         }
-	    /* info.ejs
-			<html>
-				<body>
-					<b><%= message %></b>
-					<p><a href="/">home</a></p>
-				</body>
-			</html>
-            */
-}
 
-app.get('/', (req,res) => {
+        const results = await updateDocument(db, DOCID, updateDoc);
+        res.status(200).render('info', { message: `Updated ${results.modifiedCount} document(s)` });
+    } catch (error) {
+        console.error("Error updating document:", error);
+        res.status(500).send("Internal Server Error");
+    } finally {
+        await client.close();
+    }
+};
+
+// Routes
+app.get('/', (req, res) => {
     res.redirect('/find');
-})
+});
 
-app.get('/find', (req,res) => {
+app.get('/find', (req, res) => {
     handle_Find(res, req.query.docs);
-})
+});
 
-app.get('/details', (req,res) => {
+app.get('/details', (req, res) => {
     handle_Details(res, req.query);
-})
+});
 
-app.get('/edit', (req,res) => {
+app.get('/edit', (req, res) => {
     handle_Edit(res, req.query);
-})
+});
 
-app.post('/update', (req,res) => {
+app.post('/update', (req, res) => {
     handle_Update(req, res, req.query);
-})
+});
 
-app.get('/*', (req,res) => {
-    //res.status(404).send(`${req.path} - Unknown request!`);
-    res.status(404).render('info', {message: `${req.path} - Unknown request!` });
-    /* info.ejs
-	<html>
-		<body>
-			<b><%= message %></b>
-			<p><a href="/">home</a></p>
-		</body>
-	</html>
-    */
-})
+app.get('/*', (req, res) => {
+    res.status(404).render('info', { message: `${req.path} - Unknown request!` });
+});
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+// Start the server
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
